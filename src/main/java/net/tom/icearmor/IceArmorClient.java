b3508483ea908ccfcb37d24430359242d738f4b6
Tom <tomridder061@gmail.com>
2026-01-25 11:51:19 +0100

    Initial commit

---
+++ src/main/java/net/tom/icearmor/IceArmorClient.java
@@ -0,0 +1,10 @@
+package net.tom.icearmor;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IceArmorClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/tom/icearmor/IceArmorDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tom.icearmor;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IceArmorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
