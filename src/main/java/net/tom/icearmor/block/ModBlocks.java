ed91d794b27f5b4a935f5dad87f4d3e0ff307f9b
Tom <tomridder061@gmail.com>
2026-01-25 19:35:25 +0100

    update with ores and smelting

--- src/main/java/net/tom/icearmor/block/ModBlocks.java
+++ src/main/java/net/tom/icearmor/block/ModBlocks.java
@@ -3,6 +3,7 @@ package net.tom.icearmor.block;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -10,16 +11,19 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.tom.icearmor.IceArmor;
 
 public class ModBlocks {
     public static final Block ICE_ORE = registerBlock("ice_ore",
-            new Block(AbstractBlock.Settings.create().strength(4f)
-                    .requiresTool().sounds(BlockSoundGroup.STONE).solid()));
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(2f).requiresTool().sounds(BlockSoundGroup.STONE)));
 
     public static final Block DEEPSLATE_ICE_ORE = registerBlock("deepslate_ice_ore",
-            new Block(AbstractBlock.Settings.create().strength(6f)
-                    .requiresTool().sounds(BlockSoundGroup.DEEPSLATE).solid()));
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(3f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
